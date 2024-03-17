ecb23d9ddbd9f40843b7af6bd3a6a6d23d953717
jnisperuza <jnisperuza1102@gmail.com>
2024-03-17 10:41:33 -0500

    updated allowed methods

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
